4ab174b4624ad410cbb73fea5a2a4cc40d18766f
João Campos <joaobcampos@hotmail..com>
2017-11-17 11:36:53 +0000

    graphical interface ready for use

--- build/bin/read_data.py
+++ build/bin/read_data.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
 
 def plot_info(full_data, x_axis, y_axis):
     kys = full_data.keys()
-    clr = 0;
+    colors = cm.rainbow(np.linspace(0,1, len(kys)))
+    clr = 0
     for i in kys:
-        plt.scatter(full_data[i][x_axix], full_data[i][y_axis], label=i, c=clr)
-        clr++
-    plt.show()
-
+        plt.scatter(full_data[i][x_axis], full_data[i][y_axis], label=i,
+                    color=colors[clr])
+        clr = clr + 1
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.legend()
+    #plt.show()
+    plt.savefig(y_axis.strip() + '.png')
+    plt.close()
+    
 def main():
     df = pd.read_csv("data.csv");
     print(df)
@@ -26,6 +35,10 @@ def main():
         new_info = pd.DataFrame(columns=column_names[1:(len(column_names))])
         for j in noise_levels:
             stat = df[df[column_names[0]] == i][(df[column_names[1]] > j - 0.1) & (df[column_names[1]] < j + 0.1)].mean()
+            count_info = df[df[column_names[0]] == i][(df[column_names[1]] > j - 0.1) & (df[column_names[1]] < j + 0.1)].shape
+            print("Count")
+            print("method: " + i + " noise level: " + str(j))
+            print(count_info)
             #print(stat)
             new_info = new_info.append(stat, ignore_index=True)
         print(new_info)
@@ -35,7 +48,10 @@ def main():
         print("**************")
         print(i)
         print(statistical_info[i])
-        
+    
+    plot_info(statistical_info, column_names[1], column_names[2])
+    plot_info(statistical_info, column_names[1], column_names[3])
+    plot_info(statistical_info, column_names[1], column_names[4])
 if __name__ == "__main__":
     main()
 
